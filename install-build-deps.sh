13be337be8afa056344b8591481f5e09abf3351a
Bryan Gonzalez <bgonza868@gmail.com>
2023-12-19 18:46:25 -0500

    Delete install-build-deps.sh

--- install-build-deps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Downloading dotnet-install script"
-wget https://dot.net/v1/dotnet-install.sh -O /tmp/dotnet-install.sh
-
-echo "Installing .NET 8"
-bash /tmp/dotnet-install.sh --channel 8.0
